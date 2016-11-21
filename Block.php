86a22f3fd9011fbce883061e4a3d21e10dd9b2d7
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-21 11:21:41 +0300

    1.0.1

--- Block.php
+++ Block.php
@@ -9,13 +9,13 @@ class Block extends AbstractBlock {
 	 * @see AbstractBlock::_toHtml()
 	 * @return string
 	 */
-	protected function _toHtml() {
-		return df_x_magento_init('Dfe_Customer/main', [
+	protected function _toHtml() {return
+		df_x_magento_init(__CLASS__, 'main', [
 			'countries' => df_country_codes_allowed()
 			,'telephone' => SA::s()->telephone()
 			,'utils' => df_asset_create(df_asset_name(df_asset_third_party(
 				'Telephone/js/utils.js'
 			)))->getUrl()
-		]) . df_link_inline(df_asset_name(df_asset_third_party('Telephone/css/main.css')));
-	}
+		]) . df_link_inline(df_asset_name(df_asset_third_party('Telephone/css/main.css')))
+	;}
 }
