56b95e331c1142db10cfe0b9a7083c0dd1b46069
Dmitry Fedyuk <dfediuk@gmail.com>
2018-11-13 07:28:04 +0600

    no message

--- Plugin/Customer/Model/Address/AbstractAddress.php
+++ Plugin/Customer/Model/Address/AbstractAddress.php
@@ -9,12 +9,12 @@ class AbstractAddress {
 	 * https://mage2.pro/t/1133
 	 * @see \Magento\Customer\Model\Address\AbstractAddress::validate()
 	 * @param Sb $sb
-	 * @param bool|string[] $result
+	 * @param bool|string[] $r
 	 * @return bool|string[]
 	 */
-	function afterValidate(Sb $sb, $result) {return
-		SA::s()->isTelephoneRequired(df_address_store($sb)) || !is_array($result)
-		? $result
-		: (df_clean($result, [__('Please enter the phone number.')]) ?: true)
+	function afterValidate(Sb $sb, $r) {return
+		SA::s()->isTelephoneRequired(df_address_store($sb)) || !is_array($r)
+		? $r
+		: (df_clean($r, [__('Please enter the phone number.')]) ?: true)
 	;}
 }
