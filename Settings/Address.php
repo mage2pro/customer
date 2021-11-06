d23a8ad53a58913e580db850c339a62b8371345a
Dmitry Fedyuk <dfediuk@gmail.com>
2021-11-07 05:47:33 +0600

    no message

--- Settings/Address.php
+++ Settings/Address.php
@@ -5,8 +5,7 @@ use Magento\Framework\App\ScopeInterface as S;
 /** @method static Address s() */
 final class Address extends \Df\Config\Settings {
 	/**
-	 * 2016-04-04
-	 * «Mage2.PRO» → «Customer» → «Address» → «Telephone»
+	 * 2016-04-04 «Mage2.PRO» → «Customer» → «Address» → «Telephone»
 	 * @see \Magento\Config\Model\Config\Source\Nooptreq
 	 * @param null|string|int|S $scope [optional]
 	 * @return bool
@@ -16,8 +15,7 @@ final class Address extends \Df\Config\Settings {
 	;}
 
 	/**
-	 * 2016-04-01
-	 * «Mage2.PRO» → «Customer» → «Address» → «Telephone»
+	 * 2016-04-01 «Mage2.PRO» → «Customer» → «Address» → «Telephone»
 	 * @see \Magento\Config\Model\Config\Source\Nooptreq
 	 * @param null|string|int|S $scope [optional]
 	 * @return string
